34e1859d4e2b597c01cddb9ee6d46b93d21ba90f
BEN ABBOU Marouane <maroumarocom@gmail.com>
2023-04-10 15:28:51 +0000

    Update views.py

--- network/views.py
+++ network/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
+from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import View
 from django.shortcuts import render
