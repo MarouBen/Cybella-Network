b510873daa5c7739437fbeb725c3bef9b499a829
BEN ABBOU Marouane <maroumarocom@gmail.com>
2023-04-10 14:58:40 +0000

    created ProfileView class
    the class handles showing profile data adn following or unfollowing

--- network/views.py
+++ network/views.py
@@ -1,11 +1,50 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
+from django.views import View
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User,Post
 
+@login_required
+class ProfileView(View):
+    # Get a users's profile
+    def get(self, request, username):
+        try:
+            user = User.objects.get(username=username)
+            posts = Post.objects.filter(user=user).order_by("timestamp").all()
+            picture = user.picture.url
+            return render(request, "network/profile.html", {
+                "user": user,
+                "posts": posts,
+                "picture": picture
+            })
+        except User.DoesNotExist:
+            # Handle user not found error
+            return render(request, "network/error.html", {
+                "error": "User does not exist."
+            })
+            
+    def post(self, request, username):
+        try:
+            user = User.objects.get(username=username)
+            # If the user is trying to follow or unfollow
+            # Follow a user
+            if request.POST["follow"] == "follow":
+                request.user.follow(user)
+                return HttpResponseRedirect(reverse("profile", args=[username]))
+            # Unfollow a user
+            elif request.POST["follow"] == "unfollow":
+                request.user.unfollow(user)
+                return HttpResponseRedirect(reverse("profile", args=[username]))
+        except User.DoesNotExist:
+            # Handle user not found error
+            return render(request, "network/error.html", {
+                "error": "User does not exist."
+            })
+            
 
 def index(request):
     return render(request, "network/index.html")
@@ -61,3 +100,24 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+    
+@login_required
+def new_post(request):
+    # If the user is submitting a new post
+    if request.method == "POST":
+        content = request.POST["content"]
+        user = request.user
+        post = Post(user=user, content=content)
+        post.save()
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "network/new_post.html")
+    
+def all_posts(request):
+    # Get all posts
+    posts = Post.objects.order_by("timestamp").all()
+    return render(request, "network/all_posts.html", {
+        "posts": posts
+    })
+    
+
