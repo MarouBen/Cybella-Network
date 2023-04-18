3bcc2c48d5f00f89f1cf635e409fc6c13701697d
BEN ABBOU Marouane <maroumarocom@gmail.com>
2023-04-18 02:05:23 +0000

    added a image field for the posts model

--- network/models.py
+++ network/models.py
@@ -57,6 +57,7 @@ class Post(models.Model):
     content = models.CharField(max_length=500)
     timestamp = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, blank=True, related_name="liked_posts")
+    images = models.ImageField(upload_to="network/images/posts", blank=True, null=True)
 
     def serialize(self):
         return {
