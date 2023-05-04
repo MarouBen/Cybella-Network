8fa95c18ea6273becaf6095fc4d6b9d440ba52d3
BEN ABBOU Marouane <maroumarocom@gmail.com>
2023-05-04 23:17:23 +0100

    Update urls.py

--- project4/urls.py
+++ project4/urls.py
@@ -6,7 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("network.urls")),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
