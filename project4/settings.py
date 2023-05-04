e0762e60a2f63923ef26603569039510db9dead1
BEN ABBOU Marouane <maroumarocom@gmail.com>
2023-05-04 23:25:03 +0100

    Update settings.py

--- project4/settings.py
+++ project4/settings.py
@@ -71,7 +71,7 @@ WSGI_APPLICATION = 'project4.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
